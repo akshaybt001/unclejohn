9e1f34eae700f0f67266bfe01ad2408675a03dd5
akshaybt001 <142494035+akshaybt001@users.noreply.github.com>
2023-08-25 16:19:52 +0530

    done

---
+++ main.js
@@ -0,0 +1,8 @@
+function sideNav(){
+    var dis = document.getElementById("toggle")
+    if(dis.style.display=="none"){
+        dis.style.display = "block"
+    }else{
+        dis.style.display = "none"
+    }
+}
